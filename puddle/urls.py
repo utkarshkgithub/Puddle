7d323676423591abd350f4b5777e9d728d442291
Utkarsh <152621458+utkarshkgithub@users.noreply.github.com>
2024-12-21 22:23:13 +0530

    'Add functionality to home page'

--- puddle/urls.py
+++ puddle/urls.py
@@ -11,15 +11,17 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/',admin/item/item/1/change/ include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import index,contact
 
 urlpatterns = [
+    path('items/',include('item.urls')),
     path("admin/", admin.site.urls),
     path("",index,name='index'),
     path('contact/',contact,name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
