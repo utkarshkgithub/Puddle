e2ddca6cb06583ff7b60314682fb7f4850c9aa6c
Utkarsh <152621458+utkarshkgithub@users.noreply.github.com>
2024-12-25 22:43:26 +0530

    'Add login, Sign up page '

--- puddle/urls.py
+++ puddle/urls.py
@@ -17,11 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from core.views import index,contact
 
 urlpatterns = [
+    path("",include('core.urls')),
     path('items/',include('item.urls')),
     path("admin/", admin.site.urls),
-    path("",index,name='index'),
-    path('contact/',contact,name='contact'),
+    path("login/",include('core.urls'))
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
