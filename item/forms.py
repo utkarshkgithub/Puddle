b7f804ac06973505f1f0244f63e742e1f0c72b6a
Utkarsh <152621458+utkarshkgithub@users.noreply.github.com>
2024-12-27 22:27:19 +0530

    'Add login Features'

--- item/forms.py
+++ item/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 from .models import Item
 
+Input_Class = 'w-full py-4 px-6 rounded-xl border bg-white'
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('category', 'name' ,'descripion', 'price' ,'image',)
+        fields = ('category', 'name' ,'description', 'price' ,'image',)
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': Input_Class
+            }),
+            'name': forms.TextInput(attrs={
+                'class': Input_Class
+            }),
+            'description': forms.Textarea(attrs={
+                'class': Input_Class
+            }),
+            'price': forms.TextInput(attrs={
+                'class': Input_Class
+            }),
+            'image': forms.FileInput(attrs={
+                'class': Input_Class
+            }),
+        }
--- puddle/settings.py
+++ puddle/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    "item"
+    "item",
+    "dashboard"
 ]
 
 MIDDLEWARE = [
